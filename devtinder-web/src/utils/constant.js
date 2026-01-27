5d5548bc5c64bea6fb5078f168c575ec5e0803b5
Rakesh-1143 <rakeshreddy261002@gmail.com>
2026-01-27 12:06:26 +0530

    Update API URL for production

--- devtinder-web/src/utils/constant.js
+++ devtinder-web/src/utils/constant.js
@@ -1,3 +1,15 @@
 
-const BASE_URL= "http://localhost:3000"
-export default BASE_URL
+// const BASE_URL= "http://localhost:3000"
+// export default BASE_URL
+
+// devtinder-web/src/utils/constant.js
+const BASE_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://devtinder-backend-ifde.onrender.com'  
+  : 'http://localhost:3000';
+
+export default BASE_URL;
+
+
+
+
+
