76ad03f49d3ddb28a9749a6d72d68bef6f9e452f
Rakesh-1143 <rakeshreddy261002@gmail.com>
2026-01-27 14:19:28 +0530

    Prepare frontend for Render deployment

--- devtinder-web/src/utils/constant.js
+++ devtinder-web/src/utils/constant.js
@@ -1,15 +1,11 @@
-
 // const BASE_URL= "http://localhost:3000"
 // export default BASE_URL
 
 // devtinder-web/src/utils/constant.js
-const BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://devtinder-backend-ifde.onrender.com'  
-  : 'http://localhost:3000';
+
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://devtinder-backend-ifde.onrender.com"
+    : "http://localhost:3000";
 
 export default BASE_URL;
-
-
-
-
-
